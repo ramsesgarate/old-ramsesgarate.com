1814effd427a7f461f9ae9edf6d4faa0110c99db
Ramses G <rsgarate@gmail.com>
2020-07-20 21:08:59 -0400

    Testing other font

--- src/main.js
+++ src/main.js
@@ -7,9 +7,4 @@ import DefaultLayout from '~/layouts/Default.vue'
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function(Vue, { head }) {
   Vue.component('Layout', DefaultLayout);
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap'
-  })
-
 }
