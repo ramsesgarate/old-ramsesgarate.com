e6d3393592697d9f79c2860181827365a2f3fafd
Ramses G <rsgarate@gmail.com>
2020-07-21 19:36:32 -0400

    Add typeface-roboto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // Import main css
 
 // Import default layout so we don't need to import it to every page
+require('typeface-roboto');
 import '~/assets/style/main.scss';
-
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function() {}
