6ea041dcefd59cea291d024f33378b7675d90f5f
Ramses G <rsgarate@gmail.com>
2020-07-20 20:53:49 -0400

    Add link font in head

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import '~/assets/style/main.scss'
 import DefaultLayout from '~/layouts/Default.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-export default function(Vue) {
-  Vue.component('Layout', DefaultLayout)
+export default function(Vue, { head }) {
+  Vue.component('Layout', DefaultLayout);
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Roboto'
+  })
+
 }
